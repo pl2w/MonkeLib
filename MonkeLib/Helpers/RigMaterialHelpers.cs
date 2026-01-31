f6ee183365e21badcb196fba96288fc4009e3b86
pl2w <dc.pl2w@gmail.com>
2026-01-31 12:51:40 +0200

    rig material helpers

---
+++ MonkeLib/Helpers/RigMaterialHelpers.cs
@@ -0,0 +1,32 @@
+namespace MonkeLib.Helpers;
+
+public enum GameModeMaterials
+{
+    Default = 0,
+    // Infection
+    Tagged = 1,
+    InfectionTagged = 2,
+    // Hunt
+    Hunt = 3,
+    // PaintBrawl
+    // Red Team
+    PaintBrawlRedTeam = 4,
+    PaintBrawlRedTeamHit = 5,
+    PaintBrawlRedTeamStunned = 6,
+    PaintBrawlRedTeamEliminated = 7,
+    // Blue Team
+    PaintBrawlBlueTeam = 8,
+    PaintBrawlBlueTeamHit = 9,
+    PaintBrawlBlueTeamStunned = 10,
+    PaintBrawlBlueTeamEliminated = 11,
+    // No Team
+    PaintBrawlNoTeam = 0,
+    PaintBrawlNoTeamHit = 1,
+    PaintBrawlNoTeamStunned = 17,
+    PaintBrawlNoTeamEliminated = 16,
+    // Extras
+    SodaInfected = 12,
+    GhostSkeleton = 13,
+    IceBody = 14,
+    PropHuntGorillaGhostBody = 15
+}
